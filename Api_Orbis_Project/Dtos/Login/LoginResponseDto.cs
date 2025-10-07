1bb3edc25d19585cbe7177a778fdcd5730d47bb8
Kevin7819 <kvenegasbermudez@gmail.com>
2025-10-06 23:18:35 -0600

    [06/10/2025 23:18h] Fix use email for login.

--- Api_Orbis_Project/Dtos/Login/LoginResponseDto.cs
+++ Api_Orbis_Project/Dtos/Login/LoginResponseDto.cs
@@ -3,7 +3,7 @@ namespace Api_Orbis_Project.Dtos
     public class LoginResponseDto
     {
         public int id { get; set; }
-        public string userName { get; set; } = null!;
+        public string name { get; set; } = null!;
         public string email { get; set; } = null!;
         public string token { get; set; } = null!;
         public string? role { get; set; }
